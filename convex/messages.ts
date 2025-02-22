8307d9230f7fa8316c37f498ead068436090d623
Anand <anand.gat@hotmail.com>
2025-02-23 00:33:13 +0530

    Thread Messages

--- convex/messages.ts
+++ convex/messages.ts
@@ -62,6 +62,86 @@ const getMember = async (ctx: QueryCtx, workspaceId: Id<"workspaces">, userId: I
         .unique();
 };
 
+export const getById = query({
+    args: {
+        id: v.id("messages"),
+    },
+    handler: async (ctx, args) => {
+
+        const userId = await auth.getUserId(ctx);
+        if (!userId) {
+            return null;
+        }
+
+        const message = await ctx.db.get(args.id);
+
+        if (!message) {
+            return null;
+        }
+
+        const currentMember = await getMember(ctx, message.workspaceId, userId);
+
+        if (!currentMember) {
+            return null;
+        }
+
+        const member = await populateMember(ctx, message.memberId);
+
+        if (!member) {
+            return null;
+        }
+
+        const user = await populateUser(ctx, member.userId);
+        if (!user) {
+            return null;
+        }
+
+        const reactions = await populateReactions(ctx, message._id);
+
+        const reactionsWithCounts = reactions.map((reaction) => {
+            return {
+                ...reaction,
+                count: reactions.filter((r) => r.value === reaction.value).length,
+            };
+        });
+
+        const dedupedReactions = reactionsWithCounts.reduce(
+            (acc, reaction) => {
+                const existingReaction = acc.find(
+                    (r) => r.value === reaction.value,
+                );
+
+                if (existingReaction) {
+                    existingReaction.memberIds = Array.from(
+                        new Set([...existingReaction.memberIds, reaction.memberId])
+                    );
+                } else {
+                    acc.push({ ...reaction, memberIds: [reaction.memberId] });
+                }
+                return acc;
+            },
+            [] as (Doc<"reactions"> & {
+                count: number;
+                memberIds: Id<"members">[];
+            })[]
+        );
+
+        const reactionWithoutMemberIdProperty = dedupedReactions.map(
+            ({ memberId, ...rest }) => rest,
+        );
+
+        return {
+            ...message,
+            image: message.image
+                ? await ctx.storage.getUrl(message.image)
+                : undefined,
+            user,
+            member,
+            reactions: reactionWithoutMemberIdProperty,
+        };
+    },
+});
+
 export const get = query({
     args: {
         channelId: v.optional(v.id("channels")),
@@ -165,6 +245,64 @@ export const get = query({
             )
         };
 
+    },
+});
+
+export const update = mutation({
+    args: {
+        id: v.id("messages"),
+        body: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const message = await ctx.db.get(args.id);
+        if (!message) {
+            throw new Error("Message not found");
+        }
+
+        const member = await getMember(ctx, message.workspaceId, userId);
+
+        if (!member || member._id !== message.memberId) {
+            throw new Error("Unauthorized");
+        }
+
+        await ctx.db.patch(args.id, {
+            body: args.body,
+            updatedAt: Date.now(),
+        });
+
+        return args.id;
+    },
+});
+
+export const remove = mutation({
+    args: {
+        id: v.id("messages"),
+    },
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const message = await ctx.db.get(args.id);
+        if (!message) {
+            throw new Error("Message not found");
+        }
+
+        const member = await getMember(ctx, message.workspaceId, userId);
+
+        if (!member || member._id !== message.memberId) {
+            throw new Error("Unauthorized");
+        }
+
+        await ctx.db.delete(args.id);
+
+        return args.id;
     },
 });
 
